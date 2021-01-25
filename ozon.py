5064aeff89aee4356070bbb2ee1d3922d3f55cb0
Matwey V. Kornilov <matwey.kornilov@gmail.com>
2021-01-25 12:03:24 +0300

    Update problem description

--- ozon.py
+++ ozon.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('longitude', metavar='LON', type=float, help='Longitude, deg')
+parser.add_argument('latitude',  metavar='LAT', type=float, help='Latitude, deg')
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(args.longitude, args.latitude)
